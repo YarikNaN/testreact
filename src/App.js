acbc1da75a20c6bef5e007d58c115d5fc3736c44
Dragik <106893914+YarikNaN@users.noreply.github.com>
2024-07-17 23:54:13 +0700

    did bullshit but its okay

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
-import './App.css';
-import {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
-import Header from "./components/Header/Header";
-import {Route, Routes} from 'react-router-dom'
-import ProductList from "./components/ProductList/ProductList";
-import Form from "./components/Form/Form";
+import { useEffect, useRef, useReducer } from 'react';
 
-function App() {
-    const {onToggleButton, tg} = useTelegram();
+import { reducer, initialState } from './reducer';
+import Clicker from './Clicker';
+import Autoclicker from './Autoclicker';
+
+const App = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const callback = useRef();
 
     useEffect(() => {
-        tg.ready();
-    }, [])
+        callback.current = () => {
+            const currentClics = state.clicks.amount;
+            const totalCount = Object.keys(state)
+                .reduce(
+                    (acc, cur, idx) => idx === 0
+                        ? acc
+                        : acc + state[cur].amount * (state[cur].cost * 0.1),
+                    currentClics,
+                );
+
+            dispatch({ type: 'update', payload: totalCount });
+        };
+    }, [state]);
+
+    useEffect(() => {
+        const interval = setInterval(() => callback.current(), 1000);
+        return () => clearInterval(interval);
+    }, [callback]);
 
     return (
-        <div className="App">
-            <Header />
-            <Routes>
-                <Route index element={<ProductList />}/>
-                <Route path={'form'} element={<Form />}/>
-            </Routes>
+        <div className="game">
+            <div className="banner">
+                This is an accompanying example to a clicker game tutorial using React. See
+                <a href="https://github.com/Alaricus/clicker-tutorial-react">GitHub</a>
+                for more details.
+            </div>
+            <Clicker amount={state.clicks.amount} dispatch={dispatch} />
+            {
+                Object.keys(state).map((tier, idx) => {
+                    if (idx === 0) { return null; }
+                    const { cost, amount } = state[tier];
+                    return (state.clicks.amount >= cost || amount > 0) && (
+                        <Autoclicker
+                            key={tier}
+                            tier={tier}
+                            amount={amount}
+                            cost={cost}
+                            enabled={state.clicks.amount >= cost}
+                            dispatch={dispatch}
+                        />
+                    );
+                })
+            }
         </div>
     );
-}
+};
 
 export default App;
